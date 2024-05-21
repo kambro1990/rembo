4645269680eed7f744d8fc991d895673be0c2ed7
Юрий <workjava@yandex.ru>
2024-05-21 22:53:10 +0300

    Initial commit

---
+++ eurika-client/src/main/java/eureka/eurikaclient/EurikaClientApplication.java
@@ -0,0 +1,15 @@
+package eureka.eurikaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurikaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurikaClientApplication.class, args);
+    }
+
+}
---
+++ eurika-client/src/test/java/eureka/eurikaclient/EurikaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package eureka.eurikaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
